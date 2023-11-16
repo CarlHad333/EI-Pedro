270231d07bfa6b82df0c81818df8d2d8d74f37bf
Carl <carl123haddad@gmail.com>
2023-11-16 09:16:19 +0100

    HTTP Examples

--- src/main/java/com/example/ei/EiApplication.java
+++ src/main/java/com/example/ei/EiApplication.java
@@ -1,22 +1,17 @@
 package com.example.ei;
 
+import com.google.api.client.json.Json;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.bluetooth.*;
-import javax.microedition.io.Connector;
-import javax.microedition.io.StreamConnection;
-import javax.microedition.io.StreamConnectionNotifier;
-import java.io.*;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class EiApplication {
-
-	protected static final Object lock = new Object();
-
 	public static void main(String[] args) {
 			SpringApplication.run(EiApplication.class, args);
 		}
@@ -24,20 +19,30 @@ public class EiApplication {
 
 // Add the controller.
 @RestController
-class HelloWorldController {
+class DataController {
 	@GetMapping("/")
-	public String hello() {
-
+	public String savedata() {
 		String res;
 		JdbcMysqlJ8IntegrationTests j = new JdbcMysqlJ8IntegrationTests();
 		try {
 			j.setUpPool();
 			res = j.pooledConnectionTest();
 			j.dropTableIfPresent();
-
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 		return res;
 	}
+
+	@GetMapping("/lol")
+	public String recieveData1() {
+		return "Data 1";
+	}
+
+	@PostMapping("/lol")
+	public String recieveData(@RequestBody String data) {
+		System.out.println("Received data: " + data);
+		return "Data received successfully!";
+	}
+
 }
